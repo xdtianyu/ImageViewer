ea91d374b55495157295c1524d0a003bb96b8183
tianyu <xdtianyu@gmail.com>
2015-04-30 20:49:02 +0800

    fix code

--- app/src/main/java/org/xdty/imageviewer/activity/MainActivity.java
+++ app/src/main/java/org/xdty/imageviewer/activity/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
 
         // handle only single tap event, show or hide systemUI
         mClickDetector = new android.view.GestureDetector(this,
-                new android.view.GestureDetector.SimpleOnGestureListener(){
+                new android.view.GestureDetector.SimpleOnGestureListener() {
                     @Override
                     public boolean onSingleTapConfirmed(MotionEvent e) {
                         if (isSystemUIVisible()) {
@@ -93,18 +93,18 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
             @Override
             public boolean onRotate(RotateGestureDetector detector) {
 
-                if (photoView!=null) {
+                if (photoView != null) {
                     float degree = detector.getRotationDegreesDelta();
-                    photoView.setRotation(-degree+rotate);
+                    photoView.setRotation(-degree + rotate);
                 }
                 return false;
             }
 
             @Override
             public boolean onRotateBegin(RotateGestureDetector detector) {
-                photoView = (PhotoView)mViewPager.findViewWithTag(mViewPager.getCurrentItem());
-                if (photoView!=null) {
-                    int position = (int)photoView.getTag();
+                photoView = (PhotoView) mViewPager.findViewWithTag(mViewPager.getCurrentItem());
+                if (photoView != null) {
+                    int position = (int) photoView.getTag();
                     if (rotationMap.containsKey(position)) {
                         rotate = rotationMap.get(position);
                     }
@@ -114,13 +114,13 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
 
             @Override
             public void onRotateEnd(RotateGestureDetector detector) {
-                if (photoView!=null) {
+                if (photoView != null) {
                     // set rotation and save state
-                    float degree = -detector.getRotationDegreesDelta()+rotate;
+                    float degree = -detector.getRotationDegreesDelta() + rotate;
 
-                    int n = Math.round(degree/90);
-                    n = n%4;
-                    if (n<0) {
+                    int n = Math.round(degree / 90);
+                    n = n % 4;
+                    if (n < 0) {
                         n += 4;
                     }
                     switch (n) {
@@ -144,7 +144,7 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
             }
         });
 
-        mViewPager = (JazzyViewPager)findViewById(R.id.viewpager);
+        mViewPager = (JazzyViewPager) findViewById(R.id.viewpager);
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -190,12 +190,12 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
     @Override
     public void onBackPressed() {
         //Log.d(TAG, "onBackPressed");
-        if (mViewPager.getVisibility()==View.VISIBLE) {
+        if (mViewPager.getVisibility() == View.VISIBLE) {
             mViewPager.setVisibility(View.GONE);
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
             mViewPager.setAdapter(null);
             showSystemUI();
-        } else if (mPathStack.size()>0) {
+        } else if (mPathStack.size() > 0) {
             mCurrentPath = mPathStack.pop();
             updateFileGrid();
         } else {
@@ -262,7 +262,7 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
                     // sort by filename
                     Arrays.sort(lists, SmbFileHelper.NAME_COMPARATOR);
 
-                    for (SmbFile s:lists) {
+                    for (SmbFile s : lists) {
                         //Log.d(TAG, s.getName());
                         // only show images and directories
                         if (s.isDirectory() || s.isFile() && Utils.isImage(s.getName())) {
@@ -291,8 +291,8 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
                     final Bitmap tmpBitmap = BitmapFactory.decodeStream(inputStream);
                     final Bitmap bitmap;
 
-                    if (tmpBitmap.getHeight()>tmpBitmap.getWidth()) {
-                        bitmap =RotateBitmap(tmpBitmap, -90);
+                    if (tmpBitmap.getHeight() > tmpBitmap.getWidth()) {
+                        bitmap = RotateBitmap(tmpBitmap, -90);
                     } else {
                         bitmap = tmpBitmap;
                     }
@@ -334,6 +334,27 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
 
     }
 
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    private void showSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    private boolean isSystemUIVisible() {
+        return (getWindow().getDecorView().getSystemUiVisibility() & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0;
+    }
+
     private class ViewPagerAdapter extends PagerAdapter {
 
         private ArrayList<SmbFile> fileList;
@@ -366,34 +387,13 @@ public class MainActivity extends Activity implements ViewPager.OnPageChangeList
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View)object);
+            container.removeView((View) object);
         }
 
         @Override
         public boolean isViewFromObject(View view, Object o) {
-            return view==o;
+            return view == o;
         }
-    }
-
-    private void hideSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    }
-
-    private void showSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-    }
-
-    private boolean isSystemUIVisible() {
-        return (getWindow().getDecorView().getSystemUiVisibility() & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0;
     }
 
 }
