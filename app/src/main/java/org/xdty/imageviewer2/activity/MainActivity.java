f022f1e049bc0f3a2dcfe49b5136d9ff42887080
tianyu <xdtianyu@gmail.com>
2015-05-28 21:09:54 +0800

    fix code

--- app/src/main/java/org/xdty/imageviewer2/activity/MainActivity.java
+++ app/src/main/java/org/xdty/imageviewer2/activity/MainActivity.java
@@ -996,14 +996,14 @@ public class MainActivity extends Activity
             if (actionBar != null) {
                 actionBar.show();
             }
-            
+
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
         }
         getWindow().getDecorView().setSystemUiVisibility(flags);
     }
